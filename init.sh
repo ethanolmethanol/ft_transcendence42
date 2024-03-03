861ab674f8cbc5db346f54f6e91dcdef47e14fa0
ethanolmethanol <56796479+ethanolmethanol@users.noreply.github.com>
2024-03-03 19:49:35 +0100

    Init script, but more readable (and untested).

--- init.sh
+++ init.sh
@@ -1,1 +1,9 @@
-cd back/ ; python3 -m venv venv ; source venv/bin/activate ; pip install --upgrade pip ; pip install django django-cors-headers django-hurricane psycopg2
+#!/bin/bash
+sudo apt-get install -y postgresql-client
+cd back/ 
+python3 -m venv venv 
+source venv/bin/activate 
+pip install --upgrade pip 
+pip install django django-cors-headers
+pip install django-hurricane 
+pip install psycopg2
