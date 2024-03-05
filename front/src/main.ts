536f9b3237c955500cf8ff04171857f2434f03e4
romann-broque <kinoide56@gmail.com>
2024-03-05 16:20:05 +0100

    first front main page

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/Pages/defaultPage/app.component';
+import { RootComponent } from './app/Pages/mainPage/root.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+bootstrapApplication(RootComponent, appConfig)
   .catch((err) => console.error(err));
