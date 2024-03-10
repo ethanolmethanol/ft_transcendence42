c0ccf0c95b266250e162ecff600dc488c00e3c09
romann-broque <kinoide56@gmail.com>
2024-03-10 02:04:13 +0100

    login page

---
+++ front/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.css'
+})
+
+export class SignInComponent implements OnInit {
+  signInForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.signInForm.valid) {
+      console.log("Valid authentication : " + this.signInForm.value);
+    }
+  }
+}
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/Pages/defaultPage/app.component';
-import { RootComponent } from './app/Pages/mainPage/root.component';
+import { LoginPageComponent } from './app/Pages/login-page/login-page.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-bootstrapApplication(RootComponent, appConfig)
+bootstrapApplication(LoginPageComponent, appConfig)
   .catch((err) => console.error(err));
