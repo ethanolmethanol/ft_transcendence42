84aaf04dd993b18a2f6cdcd4a47ffddbc43b0aa8
romann-broque <kinoide56@gmail.com>
2024-03-19 17:54:05 +0100

    fix status-icon alignement with input fields

--- front/src/app/components/status-icon/status-icon.component.spec.ts
+++ front/src/app/components/status-icon/status-icon.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestStatusIconComponent } from './status-icon.component';
+import {StatusIconComponent} from './status-icon.component';
 
 describe('StatusIconComponent', () => {
   let component: StatusIconComponent;
@@ -11,7 +11,7 @@ describe('StatusIconComponent', () => {
       imports: [StatusIconComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(StatusIconComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
