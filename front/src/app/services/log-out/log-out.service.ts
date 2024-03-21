11fcfe146e80783b7dce8f905b99ff67fc63fc9d
romann-broque <kinoide56@gmail.com>
2024-03-21 15:29:32 +0100

    some clean

--- front/src/app/services/log-out/log-out.service.ts
+++ front/src/app/services/log-out/log-out.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Router} from "@angular/router";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import { Observable } from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +12,10 @@ export class LogoutService {
   constructor(private router: Router, private http: HttpClient) {}
 
   public logout(): void {
-    // Make the HTTP request with the headers
     this.processLogout().subscribe();
-    // this.removeCookie('csrftoken');
-    // this.removeCookie('sessionid');
   }
 
   private processLogout() {
     return this.http.post(`${this.apiUrl}/logout/`, {}, { withCredentials: true });
-  }
-  private removeCookie(name: string) {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
   }
 }
