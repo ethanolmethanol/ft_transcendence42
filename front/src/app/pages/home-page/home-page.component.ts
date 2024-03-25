00ea007ddecdd8ca20b3e1842e37c0a3766607e5
ethanolmethanol <56796479+ethanolmethanol@users.noreply.github.com>
2024-03-25 18:17:24 +0100

    Made banner component that uses logout component (and that has a button as a placeholder until the 'profile' component exists). Now need to add scrolling and leaderboard (placeholder).

--- front/src/app/pages/home-page/home-page.component.ts
+++ front/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import {LogoutComponent} from "./logout/logout.component";
+import {RouterLink} from "@angular/router";
+import {BannerComponent} from "./banner/banner.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    LogoutComponent
+    BannerComponent,
+    RouterLink
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
