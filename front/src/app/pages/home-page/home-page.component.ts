2d0197ef9feff536afe9a69ea89147edaba42d08
ethanolmethanol <56796479+ethanolmethanol@users.noreply.github.com>
2024-03-26 11:30:48 +0100

    Managed to make a leaderboard div placeholder that requires scrolling down.

--- front/src/app/pages/home-page/home-page.component.ts
+++ front/src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {BannerComponent} from "./banner/banner.component";
+import {LeaderboardComponent} from "./leaderboard/leaderboard.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
+    RouterLink,
     BannerComponent,
-    RouterLink
+    LeaderboardComponent,
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
