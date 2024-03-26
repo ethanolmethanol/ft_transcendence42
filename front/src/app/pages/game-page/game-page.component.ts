5510176fcced55f856f92801356984b7efde8a4a
romann-broque <kinoide56@gmail.com>
2024-03-26 18:28:45 +0100

    add 'Give up' button

--- front/src/app/pages/game-page/game-page.component.ts
+++ front/src/app/pages/game-page/game-page.component.ts
@@ -1,6 +1,6 @@
 import {Component, HostListener, ElementRef, OnInit, AfterViewInit} from '@angular/core';
 import {PaddleComponent} from "../../components/paddle/paddle.component";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {GameComponent} from "./game/game.component";
 
 @Component({
@@ -15,7 +15,7 @@ import {GameComponent} from "./game/game.component";
   styleUrl: './game-page.component.css'
 })
 export class GamePageComponent implements OnInit, AfterViewInit {
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef, private router: Router) {}
 
   @HostListener('window:resize', ['$event'])
   private onResize(event: Event) {
@@ -34,5 +34,11 @@ export class GamePageComponent implements OnInit, AfterViewInit {
     const gameContainer = this.elementRef.nativeElement.querySelector('.game-container');
     const scale = Math.min(window.innerWidth / 1000, window.innerHeight / 1000);
     gameContainer.style.transform = `scale(${scale})`;
+  }
+
+  confirmGiveUp() {
+    if (confirm('Are you sure you want to give up?')) {
+      this.router.navigate(['/home']).then(r => {});
+    }
   }
 }
