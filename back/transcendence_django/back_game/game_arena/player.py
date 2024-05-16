8d15c32eec475509f839984cf028c6d7034ef9fb
romann-broque <kinoide56@gmail.com>
2024-05-16 12:37:49 +0200

    try to add mypy checks

--- back/transcendence_django/back_game/game_arena/player.py
+++ back/transcendence_django/back_game/game_arena/player.py
@@ -6,10 +6,10 @@ GIVEN_UP = 2
 
 
 class Player:
-    def __init__(self, user_id, player_name):
-        self.user_id = user_id
-        self.player_name = player_name
-        self.score = 0
+    def __init__(self, user_id: int, player_name: str):
+        self.user_id: int = user_id
+        self.player_name: str = player_name
+        self.score: int = 0
         self.status = ENABLED
         self.last_activity_time = time.time()
 
--- back/transcendence_django/back_game/game_physics/edges.py
+++ back/transcendence_django/back_game/game_physics/edges.py
@@ -1,8 +1,10 @@
+from back_game.game_physics.position import Position
+
 class Edges:
-    def __init__(self, position, width, height):
+    def __init__(self, position: Position, width: int, height: int):
         self.update(position, width, height)
 
-    def update(self, position, width, height):
+    def update(self, position: Position, width: int, height: int):
         self.bottom = position.y + height / 2
         self.top = position.y - height / 2
         self.left = position.x - width / 2
