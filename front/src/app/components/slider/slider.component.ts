b12b578b4722e4e3e46e9f778768668d25e8c1cc
opaleboreale <opaleboreale@protonmail.com>
2024-06-09 16:22:34 +0200

    design create page done

--- front/src/app/components/slider/slider.component.ts
+++ front/src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges, Renderer2, ElementRef } from '@angular/core';
+import { Component, Input, Output, OnChanges, SimpleChanges, Renderer2, ElementRef, EventEmitter } from '@angular/core';
 import { NgFor } from '@angular/common';
 
 @Component({
@@ -9,28 +9,39 @@ import { NgFor } from '@angular/common';
   styleUrl: './slider.component.css'
 })
 export class SliderComponent implements OnChanges {
-  @Input() options: (string | number)[] = [];
-  max = 0;
-  min = 0;
-  val = 0;
+  @Input() options: (string)[] = [];
+  @Input() optionIndex: number = 0;
+  @Output() selectedOption: EventEmitter<number> = new EventEmitter<number>();
+  @Output() optionIndexChange: EventEmitter<number> = new EventEmitter<number>();
+  min: number = 0;
+  max: number = 0;
+  value: string = '';
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.options && this.options) {
-      console.log(this.options);
       this.max = this.options.length - 1;
+      this.value = this.options[this.optionIndex];
+      console.log("New slider: ", this.options, " with default value: ", this.options[this.optionIndex]);
+    }
+    if (changes.optionIndex) {
+      this.value = this.options[this.optionIndex];
+      console.log("New value: ", this.options[this.optionIndex]);
     }
   }
 
   onInput(event: any): void {
-    this.val = event.target.value;
+    this.optionIndex = Number(event.target.value);
+    this.selectedOption.emit(this.optionIndex);
+    this.optionIndexChange.emit(this.optionIndex);
+    console.log("valIndex: ", this.optionIndex, " | val: ", this.options[this.optionIndex]);
   }
-  
+
   onMouseMove(event: MouseEvent): void {
     const sliderRect = this.el.nativeElement.getBoundingClientRect();
     const cursorPos = event.clientX - sliderRect.left;
-  
+
     this.options.forEach((option, index) => {
       const optionEl = this.el.nativeElement.querySelector(`option:nth-child(${index + 1})`);
       const optionRect = optionEl.getBoundingClientRect();
@@ -44,49 +55,3 @@ export class SliderComponent implements OnChanges {
     });
   }
 }
-
-
-
-
-
-
-  
-// export class SliderComponent implements OnChanges {
-//   @Input() options: (string | number)[] = [];
-//   max = 0;
-//   min = 0;
-//   val = 0;
-//   selectedOption: number | string = null;  // New property to keep track of the selected option
-
-//   constructor(private renderer: Renderer2, private el: ElementRef) {}
-
-//   ngOnChanges(changes: SimpleChanges): void {
-//     if (changes.options && this.options) {
-//       this.max = this.options.length - 1;
-//     }
-//   }
-
-//   onInput(event: any): void {
-//     this.val = event.target.value;
-//     this.selectedOption = this.options[this.val];  // Update the selected option when the slider value changes
-//   }
-  
-//   onMouseMove(event: MouseEvent): void {
-//     const sliderRect = this.el.nativeElement.getBoundingClientRect();
-//     const cursorPos = event.clientX - sliderRect.left;
-  
-//     this.options.forEach((option, index) => {
-//       const optionEl = this.el.nativeElement.querySelector(`option:nth-child(${index + 1})`);
-//       const optionRect = optionEl.getBoundingClientRect();
-//       const optionPos = optionRect.left - sliderRect.left;
-  
-//       if (cursorPos >= optionPos && cursorPos <= optionPos + optionRect.width) {
-//         this.renderer.setStyle(optionEl, 'color', option === this.selectedOption ? 'white' : '#e5f25a');  // Yellow when the cursor is over the option, unless it's the selected option
-//         this.renderer.setStyle(optionEl, 'font-weight', option === this.selectedOption ? 'bold' : 'normal');  // Bold when the cursor is over the option, unless it's the selected option
-//       } else {
-//         this.renderer.setStyle(optionEl, 'color', option === this.selectedOption ? 'white' : 'grey');  // Grey when the cursor is not over the option, unless it's the selected option
-//         this.renderer.setStyle(optionEl, 'font-weight', option === this.selectedOption ? 'bold' : 'normal');  // Normal when the cursor is not over the option, unless it's the selected option
-//       }
-//     });
-//   }
-// }
