fa4d66535c64c74c4766b380093a152ac3cd493d
opaleboreale <opaleboreale@protonmail.com>
2024-06-12 16:39:51 +0200

    profile page created

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component
 import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { AuthGuard } from "./guards/auth.guard";
 import { GuestGuard } from "./guards/guest.guard";
 import { GamePageComponent } from "./pages/game-page/game-page.component";
@@ -13,6 +14,7 @@ import { CreateOnlineGamePageComponent } from './pages/create-online-game-page/c
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfilePageComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'local', component: MonitorPageComponent, pathMatch: 'full', canActivate: [AuthGuard], data: { gameType: 'local' } },
   { path: 'local/:channel_id/:arena_id', component: GamePageComponent, pathMatch: 'prefix', canActivate: [AuthGuard], data: { gameType: 'local' } },
   { path: 'online', component: OnlineGameSelectorPageComponent, pathMatch: 'full', canActivate: [AuthGuard], data: { gameType: 'online' } },
