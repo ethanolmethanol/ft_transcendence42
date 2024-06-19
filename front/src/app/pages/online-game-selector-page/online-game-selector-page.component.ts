410d03ff1c81fa966dd583fafd4916453e34c26a
romann-broque <kinoide56@gmail.com>
2024-06-19 02:13:24 +0200

    use the button-with-icon component in the 'Play Online'

--- front/src/app/pages/online-game-selector-page/online-game-selector-page.component.ts
+++ front/src/app/pages/online-game-selector-page/online-game-selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterLink } from '@angular/router';
 import {FormsModule} from "@angular/forms";
+import {ButtonWithIconComponent} from "../../components/button-with-icon/button-with-icon.component";
 
 @Component({
   selector: 'app-online-game-selector-page',
@@ -10,6 +11,7 @@ import {FormsModule} from "@angular/forms";
     HeaderComponent,
     RouterLink,
     FormsModule,
+    ButtonWithIconComponent,
   ],
   templateUrl: './online-game-selector-page.component.html',
   styleUrl: './online-game-selector-page.component.css'
