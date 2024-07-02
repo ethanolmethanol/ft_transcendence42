a1a7cf7d7b5c12551e8e553a40e92628d0a0827e
opaleboreale <opaleboreale@protonmail.com>
2024-07-02 14:34:23 +0200

    linter error

--- back/transcendence_django/back_user/models.py
+++ back/transcendence_django/back_user/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user: User = models.OneToOneField(User, on_delete=models.CASCADE)
-    color_config: List[str] = ArrayField(models.CharField(max_length=20), default=list)
+    user: User = models.OneToOneField(User, on_delete=models.CASCADE)  # type: ignore
+    color_config: List[str] = ArrayField(models.CharField(max_length=20), default=list)  # type: ignore
 
     def save(self, *args: Any, **kwargs: Any) -> None:
         if self.pk is not None:
