70919956ff889bc0077f549e7acc9a6c37ec038e
romann-broque <kinoide56@gmail.com>
2024-07-27 17:26:46 +0200

    changes docker-compose.yml and wait-for-it.sh

--- back/wait-for-it.sh
+++ back/wait-for-it.sh
@@ -5,8 +5,11 @@ set -e
 
 cmd="$*"
 
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3 manage.py makemigrations shared_models
 python3 manage.py migrate shared_models
+python3 manage.py showmigrations
 
 echo "command is " "$cmd"
 export DJANGO_SETTINGS_MODULE=transcendence_django.settings
