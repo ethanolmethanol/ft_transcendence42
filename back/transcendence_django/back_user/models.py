ffbf215ef96d03af3612292207a34d69d775589a
opaleboreale <opaleboreale@protonmail.com>
2024-08-01 12:04:44 +0200

    restored old changes

--- back/transcendence_django/back_user/models.py
+++ back/transcendence_django/back_user/models.py
@@ -1,22 +1,22 @@
-# from typing import Any, List
-#
-# from django.contrib.auth.models import User
-# from django.contrib.postgres.fields import ArrayField
-# from django.db import models
-#
-#
-# class Profile(models.Model):
-#     user: User = models.OneToOneField(User, on_delete=models.CASCADE)  # type: ignore
-#     color_config: List[str] = ArrayField(
-#         models.CharField(max_length=20), default=list
-#     )  # type: ignore
-#     game_settings: List[int] = ArrayField(
-#         models.IntegerField(), default=list
-#     )  # type: ignore
-#
-#     def save(self, *args: Any, **kwargs: Any) -> None:
-#         if self.pk is not None:
-#             orig: Profile = Profile.objects.get(pk=self.pk)  # pylint: disable=no-member
-#             if orig.user != self.user:
-#                 raise ValueError("User can only update their color configuration.")
-#         super().save(*args, **kwargs)
+from typing import Any, List
+
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+
+
+class Profile(models.Model):
+    user: User = models.OneToOneField(User, on_delete=models.CASCADE)  # type: ignore
+    color_config: List[str] = ArrayField(
+        models.CharField(max_length=20), default=list
+    )  # type: ignore
+    game_settings: List[int] = ArrayField(
+        models.IntegerField(), default=list
+    )  # type: ignore
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        if self.pk is not None:
+            orig: Profile = Profile.objects.get(pk=self.pk)  # pylint: disable=no-member
+            if orig.user != self.user:
+                raise ValueError("User can only update their color configuration.")
+        super().save(*args, **kwargs)
