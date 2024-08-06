e6ab41e30175417c47e71789d9f4cbe58eec617f
romann-broque <kinoide56@gmail.com>
2024-08-06 12:00:56 +0200

    found really nice donut chart

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SignInPageComponent} from "./pages/sign-in-page/sign-in-page.component";
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-root',
--- front/src/app/components/stats/time-played/time-played.component.ts
+++ front/src/app/components/stats/time-played/time-played.component.ts
@@ -1,12 +1,45 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ChartModule} from "primeng/chart";
 
 @Component({
   selector: 'app-time-played',
   standalone: true,
-  imports: [],
+  imports: [
+    ChartModule
+  ],
   templateUrl: './time-played.component.html',
   styleUrl: './time-played.component.css'
 })
-export class TimePlayedComponent {
+export class TimePlayedComponent implements OnInit {
   @Input() timePlayed: string = '';
+  data: any;
+  options: any;
+
+  ngOnInit() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    this.data = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+          hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+        }
+      ]
+    };
+
+
+    this.options = {
+      cutout: '60%',
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      }
+    };
+  }
 }
